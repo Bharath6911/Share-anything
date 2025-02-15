55ab9ac199279044e17cb48e79add6033c2806fb
Bharath <mummadibharath9966@gmail.com>
2025-02-15 21:17:15 +0530

    Update server.js

--- server.js
+++ server.js
@@ -112,7 +112,7 @@ app.post('/api/chat', async (req, res) => {
 
   try {
     const response = await axios.post(
-      'https://api-inference.huggingface.co/models/Adriana213/gpt2-xl-text-generation',
+      'https://api-inference.huggingface.co/models/meta-llama/Llama-3.2-3B-Instruct',
       { inputs: userMessage },
       {
         headers: {
